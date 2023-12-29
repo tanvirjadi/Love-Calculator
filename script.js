5bb1424ad3fbadfcbf0f63006e4530c6fa9b586c
tanvirjadi <138885932+tanvirjadi@users.noreply.github.com>
2023-12-29 19:20:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
         var loveScore = Math.floor(Math.random() * 100 + 1);
         var name1 = $(".input1").val();
         var name2 = $(".input2").val();
-        if (loveScore > 70) {
+        if (name1 === ''||name2 === '') {
+            alert("Enter Both Names.")
+        } else if (loveScore > 70) {
             $("h1").html(name1 + ' <span class="heart" style="font-size: 5rem;">&hearts;</span> ' + name2);
             $("p").html("<h2>" + loveScore + "%" + "</h2>");
             $(".para-2").text("Your loves each other is like kanye loves kanye");
